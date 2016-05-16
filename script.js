34daf065b8a9c6a442f54eab136b40da4859ab53
Webmaster <webmaster.alfamed@gmail.com>
2016-05-16 18:15:56 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+;(function(){
+    var num = 9,
+        mas1 = [],
+        n = 99,
+        tmp = 0;
+    
+    mas1.push(num);
+
+     for (var i = 2; i <= n; i++) {
+        // mas1*9
+        var tmpResult = [];
+
+        
+        for (var j = 0; j < mas1.length; j++) {
+            var element = mas1[j],
+                curMultiply = 0;
+            
+            curMultiply = element*num + tmp;
+    
+            if (curMultiply.toString().length>1) {            
+                tmpResult.push(parseInt(curMultiply.toString()[1]));
+                tmp = parseInt(curMultiply.toString()[0]);
+
+                if (j==(mas1.length-1)) {
+                    tmpResult.push(parseInt(curMultiply.toString()[0]));
+                    tmp = 0;
+                };
+
+            } else {
+                tmpResult.push(curMultiply);
+                tmp = 0;
+            };
+        }
+
+        for (var j = 0; j < tmpResult.length; j++) {
+            mas1[j] = tmpResult[j];
+        };        
+     }
+
+    //  inverting
+    var k = 0;
+    for (var j = tmpResult.length-1; j >= 0; j--) {       
+        mas1[k] = tmpResult[j];
+        k++;
+    }
+    
+    console.log(mas1);
+    
+})();
